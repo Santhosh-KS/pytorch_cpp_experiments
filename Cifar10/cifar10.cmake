05eb4f7f9765ee8fdbbb3f98b2c906fdaf720158
Santhosh K S <santhoshachar08@gmail.com>
2019-03-30 21:36:40 +0530

    Falling back on opencv3.4.3

--- Cifar10/cifar10.cmake
+++ Cifar10/cifar10.cmake
@@ -1,13 +1,14 @@
 PROJECT(Cifar10Classifier)
 
-SET(OpenCV_DIR /usr/lib/cmake/opencv4)
+#SET(OpenCV_DIR /usr/lib/cmake/opencv4)
+SET(OpenCV_DIR /usr/lib/cmake/openCV-3.4.3)
+#SET(OpenCV_DIR /home/santhosh/course/reloaded/opencv/new_custom_install/lib/cmake/opencv4)
 FIND_PACKAGE(OpenCV REQUIRED)
 
-INCLUDE_DIRECTORIES(${OpenCV_INCLUDE_DIRS})
 
 SET(SRC Cifar10/Cifar10DataSetParser.cpp
         Cifar10/Cifar10.cpp)
 
+INCLUDE_DIRECTORIES(${OpenCV_INCLUDE_DIRS})
 ADD_EXECUTABLE(Cifar10 ${SRC})
-TARGET_LINK_LIBRARIES(Cifar10 ${TORCH_LIBRARIES} ${OpenCV_LIBS})
-# TARGET_LINK_LIBRARIES(${name} ${OpenCV_LIBS} 
+TARGET_LINK_LIBRARIES(Cifar10  ${OpenCV_LIBS} ${TORCH_LIBRARIES} )
